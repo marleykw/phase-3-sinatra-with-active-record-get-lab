95b588cee908ba19ba2ba8d5dd005bc5f3ad59db
Marley Watson <marleykw17@gmail.com>
2023-03-31 15:24:33 -0400

    commit tree cdefaec7d753354d11a3a3b837fe089e49186677

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < Sinatra::Base
+  get '/bakeries' do
+  # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
 
-  # add routes
+  get '/bakeries/:id'do
+    bakeries_id = Bakery.find(params[:id])
+    bakeries_id.to_json
+  end
 
+  get 'baked_goods/by_price' do
+    by_price = Bakery.order(:price)
+    by_price.to_json
+  end
+
+  get 'baked_goodes/most_expensive' do
+  end
 end
